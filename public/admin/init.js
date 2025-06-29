7b1e1ec16079bf6be2948eb43aeffafc5ca0d069
exgael <ray-clinic.85@icloud.com>
2025-06-29 15:06:13 +0200

    fix: ID widget launch order

--- public/admin/init.js
+++
@@ -1,6 +0,0 @@
-import "https://unpkg.com/decap-cms@^3.1.0/dist/decap-cms.js";
-import "https://unpkg.com/decap-cms-widget-id@^3.0.0/dist/decap-cms-widget-id.js";
-
-CMS.registerWidget('ncw-id', DecapCMSWidgetId.IdControl);
-
-CMS.init(); 
